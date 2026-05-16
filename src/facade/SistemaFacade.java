6c87bd1a8003eead199c85e0495e137b9a95267e
Extronaz <leonardovillagomez199@gmail.com>
2026-05-16 18:37:14 -0500

    Implementacion patron Facade

---
+++ src/facade/SistemaFacade.java
@@ -0,0 +1,19 @@
+package facade;
+
+public class SistemaFacade {
+
+    public void registrarCliente() {
+
+        System.out.println("Cliente registrado");
+    }
+
+    public void registrarOrden() {
+
+        System.out.println("Orden tecnica registrada");
+    }
+
+    public void generarReporte() {
+
+        System.out.println("Reporte generado");
+    }
+}
